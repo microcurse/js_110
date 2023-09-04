011cffd32671194aa40b01c5ca184aa4e67fae82
microcurse <marc.maninang@gmail.com>
2023-09-04 14:11:33 -0700

    added PEDAC template file

---
+++ lesson_2/practice_problems/pedac_template.js
@@ -0,0 +1,22 @@
+/**
+ * PEDAC
+ * 
+ * Understand the Problem
+ * 
+ * 
+ * Explicit Requirements:
+ * - Input: 
+ * - Output:
+ * 
+ * Implicit Requirements:
+ * 
+ * Questions:
+ * 
+ * Examples and test cases
+ * 
+ * Data Structure
+ * 
+ * Algorithm
+ * 
+ * Code
+ */
