016ce32ac129d31398c1a31ff4245add048e3515
Marc <marc.maninang@gmail.com>
2023-10-06 22:21:27 -0700

    Fixing conflicts

--- small_problems/easy4/letter_swap.js
+++ small_problems/easy4/letter_swap.js
@@ -60,4 +60,4 @@ function swap(string) {
 
 console.log(swap('Oh what a wonderful day it is'));  // "hO thaw a londerfuw yad ti si"
 console.log(swap('Abcde'));                          // "ebcdA"
-console.log(swap('a'));                              // "a"
+console.log(swap('a'));                              // "a" 
