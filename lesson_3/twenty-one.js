fb747256fc7194148c0b37b27c3e5f1a893d2fc2
microcurse <marc.maninang@gmail.com>
2023-11-10 23:08:51 -0800

    Almost a complete version of the game. Still need to add some functionality. TODO listed in comments

--- lesson_3/twenty-one.js
+++ lesson_3/twenty-one.js
@@ -12,36 +12,165 @@
  * 6. If dealer busts, player wins.
  * 7. Compare cards and declare winner.
  * 
+ * TODO
+ * - Fix cardToString function
+ * - Create playAgain function
+ * - Clean up user input for hit or stay to accept 'h' or 's'
+ * - Create a graphical representation?
+ * 
  */
 
-/**
- * Data structure for the deck
- * 
- * Separate arrays
- * Pros
- * - Easy to randomly select a suit and a value for a card using a random index
- * 
- * Cons
- * - Could potentially select the same card and suit
- */
-
+const readline = require('readline-sync');
 const SUITS = ['Diamonds', 'Clubs', 'Hearts', 'Spades'];
 const VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+const WINNING_NUMBER = 21;
+const COMPUTER_STAY_NUMBER = 17;
 
-/**
- * Jack, Queen, King has a value of 10
- * Ace has a value of either 1 or 11
- * 
- */
+function prompt(msg) {
+  console.log(`=> ${msg}`);
+}
 
-const DECK = [];
+function initializeDeck() {
+  let deck = [];
 
-VALUES.forEach((value) => {
-  SUITS.forEach((suit) => {
-    DECK.push([value, suit]);
-  }); 
-});
+  VALUES.forEach((value) => {
+    SUITS.forEach((suit) => {
+      deck.push([value, suit]);
+    }); 
+  });
 
-console.log(DECK);
-console.log(DECK.length);
-console.log(DECK[0], DECK[1]);
+  return deck;
+}
+
+function shuffleDeck(array) {
+  for (let index = array.length - 1; index > 0; index--) {
+    let otherIndex = Math.floor(Math.random() * (index + 1)); // 0 to index
+    [array[index], array[otherIndex]] = [array[otherIndex], array[index]]; // swap elements
+  }
+}
+
+function calculateCardsValue(cards) {
+  let values = cards.map((card) => card[0]);
+  let sum = 0;
+  
+  values.forEach((value) => {
+    if (value === 'Ace') {
+      sum += 11;
+    } else if (['Jack', 'Queen', 'King'].includes(value)) {
+      sum += 10;
+    } else {
+      sum += Number(value);
+    }
+  });
+
+  values.filter((value) => value === 'Ace').forEach(_ => {
+    if (sum > 21) sum -= 10;
+  });
+
+  return sum;
+}
+
+function cardToString(cards) {
+  let result = '';
+  cards.forEach((card) => {
+    result += card.join(' of ');
+    result += ", "
+  });
+
+  return result;
+}
+
+function busted(total) {
+  return total > WINNING_NUMBER;
+}
+
+function playerTurn(deck, playersCards) {
+  while(true) {
+    playersCards.value = calculateCardsValue(playersCards.cards);
+    prompt(`Your cards are: ${playersCards.cards}`);
+    prompt(`Your total is: ${playersCards.value}`);
+    prompt("hit or stay?");
+
+    let answer = readline.question().toLowerCase();
+
+    if (answer === 'stay' || busted(playersCards.value)) break;
+
+    if (answer === 'hit') {
+      playersCards['cards'].push(deck.shift());
+      playersCards.value = calculateCardsValue(playersCards.cards);
+      if (busted(playersCards.value)) break;
+    }
+
+    prompt(playersCards.value);
+  }
+
+}
+
+function dealersTurn(deck, computersCards) {
+  while(true) {
+    computersCards['value'] = calculateCardsValue(computersCards.cards);
+    prompt(`The dealer has a total of: ${computersCards.value}`);
+
+    if (computersCards.value >= COMPUTER_STAY_NUMBER || busted(computersCards.value)) break;
+
+    let nextCard = deck.shift();
+
+    prompt(`Dealer hits`);
+    computersCards['cards'].push(nextCard);
+    computersCards['value'] = calculateCardsValue(computersCards.cards);
+  }
+
+}
+
+function displayResult(winner) {
+  if (winner) {
+    prompt(`The ${winner} wins!`);
+  } else {
+    prompt(`It's a tie!`);
+  }
+}
+
+function calculateResults(playersTotal, dealersTotal) {
+  if (busted(dealersTotal)) {
+    return 'Player';
+  } else if (busted(playersTotal)) {
+    return 'Dealer';
+  } else if (playersTotal < dealersTotal) {
+    return 'Dealer';
+  } else if (playersTotal > dealersTotal) {
+    return 'Player';
+  }
+
+  return null;
+}
+
+function playTwentyOne() {
+  const DECK = initializeDeck();
+  shuffleDeck(DECK);
+  const PLAYERS_CARDS = { cards: DECK.splice(0, 2), value: 0 };
+  const DEALERS_CARDS = { cards: DECK.splice(3, 2), value: 0 };
+  let winner;
+
+  playerTurn(DECK, PLAYERS_CARDS);
+
+  if (busted(PLAYERS_CARDS.value)) {
+    prompt(`Your total is: ${PLAYERS_CARDS.value}`);
+    prompt(`You busted!`);
+  } else {
+    prompt(PLAYERS_CARDS.value);
+    prompt("You chose to stay!");
+    dealersTurn(DECK, DEALERS_CARDS);
+
+    if (busted(DEALERS_CARDS.value)) {
+      prompt(`Dealer busted`);
+    } else {
+      prompt(`The dealer stays at: ${DEALERS_CARDS.value}`);
+    }
+
+  }
+
+  winner = calculateResults(PLAYERS_CARDS.value, DEALERS_CARDS.value);
+  displayResult(winner);
+}
+
+playTwentyOne();
