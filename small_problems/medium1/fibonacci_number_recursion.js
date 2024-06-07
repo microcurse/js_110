95598da86e130c097260d4b3bd5b33abc6fa00d8
microcurse <marc.maninang@gmail.com>
2024-06-06 23:30:00 -0700

    I think I had a good understanding of what's happening in this exercise. Will definitely need more practice on recurision though

---
+++ small_problems/medium1/fibonacci_number_recursion.js
@@ -0,0 +1,56 @@
+/**
+ * PEDAC
+ * 
+ * Understanding the Problem
+ * Write a recursive function that computes the nth Fibonacci number, where nth is an argument passed to the function.
+ * 
+ * Explicit requirements:
+ * - Input: a number
+ * - Output: a number
+ * - The input is the the nth Fibonacci number
+ * - Write a recursive function to acheive the output number
+ * - The output is the nth Fibonacci number
+ * 
+ * Implicit requirements:
+ * - N/A
+ * 
+ * Questions:
+ * - N/A
+ * 
+ * Examples and Test Cases
+ * fibonacci(1);       // 1
+ * fibonacci(2);       // 1
+ * fibonacci(3);       // 2
+ * fibonacci(4);       // 3
+ * fibonacci(5);       // 5
+ * fibonacci(12);      // 144
+ * fibonacci(20);      // 6765
+ * 
+ * If n is 1, fibonacci is 1
+ * If n is 2, fibonacci is 1
+ * If n is 3, previous two numbers are 1 + 1 = 2
+ * If n is 4, previous two numbers are 1 + 2 = 3
+ * If n is 5, previous two numbers are 2 + 3 = 5
+ * 
+ * Data Structure
+ * - Numbers
+ * 
+ * Algorithm
+ * - If n is 1, return 1
+ * - If n is 2, return 1
+ * - Otherwise, return fibonacci(n - 1) + fibonacci(n - 2)
+ * 
+ */
+
+function fibonacci(num) { 
+  if (num <= 2) return 1;
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+console.log(fibonacci(1));       // 1
+console.log(fibonacci(2));       // 1
+console.log(fibonacci(3));       // 2
+console.log(fibonacci(4));       // 3
+console.log(fibonacci(5));       // 5
+console.log(fibonacci(12));      // 144
+console.log(fibonacci(20));      // 6765
